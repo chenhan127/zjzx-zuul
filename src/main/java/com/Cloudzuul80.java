7a263db61b97d07c250739718e8435c20bd9cc11
chenhan <598202794@qq.com>
2019-02-23 10:20:02 +0800

    a

--- src/main/java/com/Cloudzuul80.java
+++ src/main/java/com/Cloudzuul80.java
@@ -3,11 +3,28 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 @SpringBootApplication
 @EnableZuulProxy
 public class Cloudzuul80 {
 	public static void main(String[] args) {
 		SpringApplication.run(Cloudzuul80.class, args);
+	}
+	
+	@Bean
+	public CorsFilter corsFilter() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    final CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.addAllowedOrigin("*");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("*");
+	    config.setMaxAge(18000L);
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
 	}
 }
 
